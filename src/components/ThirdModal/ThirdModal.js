7192acbdc0d5a31dde4cb35286b5a23bb3d4a77c
Felipe Rezende <felipevix@hotmail.com>
2022-03-25 13:26:58 -0400

    refactoring

--- src/components/ThirdModal/ThirdModal.js
+++ src/components/ThirdModal/ThirdModal.js
@@ -5,27 +5,24 @@ import Avatar2 from '../../assets/images/unsplash_mEZ3PoFGs_k_Avatar.svg';
 import Avatar3 from '../../assets/images/unsplash_WNoLnJo7tS8_Avatar.svg';
 import Shoe2 from '../../assets/images/unsplash_dwKiHoqqxk8Shoeimages.svg';
 import Shoe3 from '../../assets/images/unsplash_NOpsC3nWTzYShoeimages.svg';
+import CancelIcon from '../../assets/images/circle-cancel_major.svg';
 
 
 const ThirdModal = ({ closeModal }) => {
   return (
     <div className="modal-wrapper">
       <div className="modal-third">
-        <button
-          onClick={() => {
-            closeModal();
-          }}
-        >
-          Next Modal
-        </button>
+        
         <div className="modal3__container">
+          
           <div className="modal3__wrapper">
             <div className="modal3__header">
               <h1 className="modal3__title">
                 You're Insterested in Selling
               </h1>
               <form className="modal3__form">
-                <input type='text' placeholder='Shoes'></input>
+              <img className="modal3__form-cancelicon" src={CancelIcon} alt='cancel icon'></img>
+                <input className="modal3__input" type='text' placeholder='Shoes'></input>
               </form>
             </div>
             <h2 className="modal3__subtitle">Suggested shpos and mentors to get you started</h2>
@@ -34,6 +31,7 @@ const ThirdModal = ({ closeModal }) => {
                 <div className="modal3__card-img">
                   <img className="modal3__card-product" src={Shoe}></img>
                 </div>
+                <div className="card-info__wrapper">
                 <div className="modal3__card-info">
                   <p className="modal3__card-infotitle">SHOES.COM</p>
                   <div className="modal3__card-infosubtitle">
@@ -46,11 +44,13 @@ const ThirdModal = ({ closeModal }) => {
                   <button className="modal3__card-button">Learn Adrians Guide</button>
                 </div>
               </div>
+              </div>
 
               <div  className="modal3__card">
                 <div className="modal3__card-img">
                   <img className="modal3__card-product" src={Shoe2}></img>
                 </div>
+                <div className="card-info__wrapper">
                 <div className="modal3__card-info">
                   <p className="modal3__card-infotitle">SHOZELLA.COM</p>
                   <div className="modal3__card-infosubtitle">
@@ -60,14 +60,19 @@ const ThirdModal = ({ closeModal }) => {
                 </div>
                 <div className="modal3__card-infobottom">
                   <img className="modal3__card-avatar" src={Avatar2} alt='avatar icon'/>
-                  <button className="modal3__card-button">Learn Jennys Guide</button>
+                  <button
+                  onClick={() => {
+                    closeModal();
+                  }} className="modal3__card-button">Learn Jennys Guide</button>
                 </div>
+              </div>
               </div>
 
               <div  className="modal3__card">
                 <div className="modal3__card-img">
                   <img className="modal3__card-product" src={Shoe3}></img>
                 </div>
+                <div className="card-info__wrapper">
                 <div className="modal3__card-info">
                   <p className="modal3__card-infotitle">SOLES.COM</p>
                   <div className="modal3__card-infosubtitle">
@@ -79,6 +84,7 @@ const ThirdModal = ({ closeModal }) => {
                   <img className="modal3__card-avatar" src={Avatar3} alt='avatar icon'/>
                   <button className="modal3__card-button">Learn Jonys Guide</button>
                 </div>
+              </div>
               </div>
 
             </div>
