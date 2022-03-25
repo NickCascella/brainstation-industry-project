c60d3f8b3ba1289d15f12815ba00390e1df26ee7
NickCascella <83091530+NickCascella@users.noreply.github.com>
2022-03-25 01:32:17 -0400

    progressing with landing page

--- src/App.js
+++ src/App.js
@@ -6,10 +6,25 @@ import HomePage from "./pages/HomePage/HomePage";
 function App() {
   return (
     <Router>
-      <Nav />
-      <Switch>
-        <Route path={"/"} exact component={HomePage} />
-      </Switch>
+      <div className="app">
+        <div className="app__search">
+          <div className="app__shopify-logo-wrapper">
+            <img src={""} alt="shopify logo" />
+            <span className="app__shopify-username-menu">michelleserdan.u</span>
+          </div>
+          <input className="app__search-bar" type="text" placeholder="Search" />
+          <div className="app__shopify-user-icon-wrapper">
+            <img src={"/"} alt="user logo" />
+            <span className="app__shopify-username">Michelle Serdan</span>
+          </div>
+        </div>
+        <div>
+          <Nav />
+          <Switch>
+            <Route path={"/"} exact component={HomePage} />
+          </Switch>
+        </div>
+      </div>
     </Router>
   );
 }
--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,21 +1,72 @@
 import "./Nav.scss";
-import { Link } from "react-router-dom";
+import homeIcon from "../../assets/Icons/home_majoricons.svg";
+import customersIcon from "../../assets/Icons/customers_majoricons.svg";
+import productsIcon from "../../assets/Icons/products_majoricons.svg";
+import discountIcon from "../../assets/Icons/discounts_majoricons.svg";
+import appsIcon from "../../assets/Icons/apps_majoricons.svg";
+import analyticsIcon from "../../assets/Icons/Solid/analyticsicons.svg";
+import circlePlus from "../../assets/Icons/circle-plus_majoricons.svg";
+import storeIcon from "../../assets/Icons/store_majoricons.svg";
+import viewIcon from "../../assets/Icons/view_majoricons.svg";
 
 const Nav = () => {
   return (
     <nav className="nav">
       <ul className="nav__links-list">
-        <li>
-          <Link className="nav__link" to={"/"}>
-            Home
-          </Link>
+        <li className="nav__list-item">
+          <img className="nav__item-img" src={homeIcon} alt={"home"} />
+          <span className="nav__item-text">Home</span>
         </li>
-        <li>
-          <Link className="nav__link" to={"/helloTwo"}>
-            Link 2
-          </Link>
+        <li className="nav__list-item">
+          <img className="nav__item-img" src={"/"} alt={"orders"} />
+          <span className="nav__item-text">Orders</span>
+        </li>
+        <li className="nav__list-item">
+          <img className="nav__item-img" src={productsIcon} alt={"home"} />
+          <span className="nav__item-text">Products</span>
+        </li>
+        <li className="nav__list-item">
+          <img
+            className="nav__item-img"
+            src={customersIcon}
+            alt={"customers"}
+          />
+          <span className="nav__item-text">Customers</span>
+        </li>
+        <li className="nav__list-item">
+          <img
+            className="nav__item-img"
+            src={analyticsIcon}
+            alt={"analytics"}
+          />
+          <span className="nav__item-text">Analytics</span>
+        </li>
+        <li className="nav__list-item">
+          <img className="nav__item-img" src={"/"} alt={"marketing"} />
+          <span className="nav__item-text">Marketing</span>
+        </li>
+        <li className="nav__list-item">
+          <img className="nav__item-img" src={discountIcon} alt={"home"} />
+          <span className="nav__item-text">Discounts</span>
+        </li>
+        <li className="nav__list-item">
+          <img className="nav__item-img" src={appsIcon} alt={"home"} />
+          <span className="nav__item-text">App</span>
         </li>
       </ul>
+      <div className="additional-list">
+        <div className="additional-list__item">
+          <span className="additional-list__title">Sales Channels</span>
+          <img className="nav__item-img" src={circlePlus} />
+        </div>
+        <div className="additional-list__item">
+          <div className="additional-list__title-wrapper">
+            <img className="nav__item-img" src={storeIcon} />
+            <span className="additional-list__title">Sales Channels</span>
+          </div>
+          <img className="nav__item-img" src={viewIcon} />
+        </div>
+      </div>
     </nav>
   );
 };
